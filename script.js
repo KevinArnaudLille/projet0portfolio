14ed9767e659baf9a6be3f0a37f15b2381fddde9
Kevin Arnaud <kevin.arnaud.addpro@gmail.com>
2021-12-10 13:57:09 +0100

    KA

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// SEASONS SWAP BUTTONS MAIN SETTINGS -----------------------------------
-// Season object constructeur declaration
-function $(selector){
+// GLOBAL FUNCTIONS
+function $(selector) {
   return document.querySelector(selector)
 }
+
+// SEASONS SWAP BUTTONS MAIN SETTINGS -----------------------------------
+// Season object constructeur declaration
 
 function Season(id) {
   this.seasonId = id,
@@ -36,22 +38,67 @@ for (item of ["fall", "winter", "spring", "summer"]) {
 };
 
 // WINTER -------------------------------
+let snowflakesNb = 10;
 
+// Create and add a snowflake
+let snowflake = document.createElement("div");
+snowflake.setAttribute("class", "snowflake");
+snowflake.setAttribute("id", "snowflakeA");
+$("#jsAnimatedElements").append(snowflake);
+let mouseClickOnSnowflake = false;
+snowflake.addEventListener('click', () => {
+  mouseClickOnSnowflake = !mouseClickOnSnowflake
+});
+let mouseIsOnSnowflake = false;
+snowflake.addEventListener('mouseover', () => {
+  mouseIsOnSnowflake = !mouseIsOnSnowflake
+});
 
+let yPos = -10;
+let snowflakeSpeed = 0.15;
+let brownianFactor = 0.3;
+let xStartingPos = 200;
+let xMaxBrownianShift = 15;
+let xMaxMouseoverShift = 18;
+let dodgeFactor = 20;
+let xShift = 0;
+let snowflakeReverseExcitement = 8;
+
+function snowflakeMove() {
+  snowflake.style.cssText = `
+    width: 30px;
+    height: 30px;
+    left: ${xStartingPos + xShift}px;
+    top:${yPos * snowflakeSpeed}vh;
+    border-radius: 50%;
+    background-color: white;
+  `;
+
+  (yPos * snowflakeSpeed) < 110 ? yPos++ : yPos = -10;
+
+  if (Math.round(yPos) % snowflakeReverseExcitement == 0) {
+    xShift += xMaxBrownianShift * brownianFactor * (Math.random() - 0.5);
+  };
+  if (mouseIsOnSnowflake) {
+    xShift += xMaxMouseoverShift * brownianFactor * (Math.random() - 0.5);
+    yPos += dodgeFactor * (Math.random() - 0.5);
+    mouseIsOnSnowflake = !mouseIsOnSnowflake;
+  };
+  if (mouseClickOnSnowflake) {
+    snowflake.remove();
+    // ici une fonction qui genere un nouveau flocon
+  };
+}
+
+// ANIMATION ------------------------------
+
+function animate() {
+  snowflakeMove();
+  requestAnimationFrame(animate)
+}
+animate();
 
 // FALL ---------------------------------
-
-
-
-// let leaf = document.createElement("div");
-// leaf.setAttribute("class", "leaves");
-// leaf.setAttribute("id", "leafA");
-// leaf.style.cssText = `
-// width: 50px;
-// height: 50px;
-// `;
-// document.querySelector("#jsAnimatedElements").append(leaf);
-// leaf.addEventListener('mouseover', (e) => dodgeMouse(e))
 
 // let mouseOver = false
 // function dodgeMouse(e) {
@@ -60,24 +107,13 @@ for (item of ["fall", "winter", "spring", "summer"]) {
 //   setTimeout(()=>mouseOver=false,1000)
 // }
 
-// let i = 0;
-// let speed = 0.3;
+// let yPos = 0;
+// let snowflakeSpeed = 0.3;
 // function leafMove(height=50) {
 //   leaf.style.cssText = `
 //     width: 50px;
 //     height: ${height}px;
-//     top:${i * speed}vh;
+//     top:${yPos * snowflakeSpeed}vh;
 //   `;
-//   (i * speed) < 110 ? i++ : i = 0;
+//   (yPos * snowflakeSpeed) < 110 ? yPos++ : yPos = 0;
 // }
-
-// Animation
-
-// function animate() {
-//   leafMove()
-//   if (mouseOver){
-//     leafMove(70)
-//   }
-//   requestAnimationFrame(animate)
-// }
-// animate();
